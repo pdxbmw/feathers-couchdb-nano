b934c1b3442b5e498f25c93cfe3568eb7b4c388d
Andi Wilson <pdxbmw@gmail.com>
2017-04-25 08:02:09 -0400

    Cleanup error handler.

--- src/utils.js
+++ src/utils.js
@@ -2,19 +2,16 @@ import errors from 'feathers-errors';
 
 // https://github.com/feathersjs/feathers-errors
 export function errorHandler (error) {
-  let feathersError = error;
 
-  if (error.name) {
-    switch (error.name) {
-      case 'CouchError':
-        if (error.code === 404 || error.headers.status === 404) {
-          feathersError = new errors.NotFound(error);
-        } else {
-          feathersError = new errors.GeneralError(error);
-        }
-        break;
-    }
+  switch (error.name) {
+    case 'CouchError':
+      if (error.code === 404 || error.headers.status === 404) {
+        error = new errors.NotFound(error);
+      } else {
+        error = new errors.GeneralError(error);
+      }
+      break;
   }
 
-  throw feathersError;
+  throw error;
 }
